4d19d53818a6744e0e69ae8965fef89a441acc50
Susannah Zhou <zhou_susannah99@yahoo.ca>
2025-01-08 03:57:25 +0530

    create an ec2 instance

--- my-ec2/lib/my-ec2-stack.ts
+++ my-ec2/lib/my-ec2-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MyEc2Stack extends cdk.Stack {
+export class SzhouEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // define default vpc
+    const vpc = ec2.Vpc.fromLookup(this,'ExistingVPC',{
+      isDefault: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // creating ec2 instance 
+    const szhouinstance = new ec2.Instance(this,'szhoumachine',{
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyName: 'splunk-key',
+    });
+
   }
 }
