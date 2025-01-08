9fa79b3a0a6d28da02fbe180e8c29ce490afd786
Susannah Zhou <zhou_susannah99@yahoo.ca>
2025-01-08 23:30:01 +0530

    update day3 folder name to day33

---
+++ szhou-ec2vpc-day33/lib/szhou-ec2vpc-day3-stack.ts
@@ -0,0 +1,44 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class SzhouEc2VpcDay3Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // using default vpc
+    const vpc = ec2.Vpc.fromLookup(this,'szhouvpc',{
+      isDefault: true
+    });
+    // creating ec2 instance
+    const szhouvm = new ec2.Instance(this, 'szhouvm1',{
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyPair: ec2.KeyPair.fromKeyPairName(this,'szhoukey','splunk-key'),
+      //       splunk-key is original key name of aws account
+      // so you have to use the same
+      instanceName: 'szhou-linux-vm'
+      // above name of my linux machine
+    });
+
+    // printing instance ID
+    new cdk.CfnOutput(this, 'szhouInstanceID', {
+      description: 'This will print instance ID: ',
+      value: szhouvm.instanceId,
+    });
+    // printing public dns
+    new cdk.CfnOutput(this, 'szhouvmpublicdns',{
+      description: 'This will print instance DNS name: ',
+      value: szhouvm.instancePublicDnsName,
+    });
+    new cdk.CfnOutput(this, 'szhouvmpublicIp',{
+      description: 'This will print instance public IP: ',
+      value: szhouvm.instancePublicIp,
+    });
+
+  
+  }
+}
+
---
+++ szhou-ec2vpc-day33/test/szhou-ec2vpc-day3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SzhouEc2VpcDay3 from '../lib/szhou-ec2vpc-day3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/szhou-ec2vpc-day3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SzhouEc2VpcDay3.SzhouEc2VpcDay3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
